6fc359b4a230592098f76d54434dbdd28c5c7c73
Andrey <ckastrusshion@gmail.com>
2024-02-07 14:12:12 +0300

    add movie card and trends

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,11 +5,32 @@
 
 import { sidebar } from "./sidebar.js";
 import { api_key, imageBaseURL, fetchDataFromServer } from "./api.js";
+import { createMovieCard } from "./movie-card.js"
 
 const pageContent = document.querySelector("[page-content]")
 
 
 sidebar();
+
+//Home page sections
+
+
+const homePageSections = [
+    {
+        title: "Upcoming Movies",
+        path: "/movie/upcoming"
+    },
+    {
+        title: "Today\'s Trending Movies",
+        path: "/trending/movie/week"
+    },
+    {
+        title: "Top Rated Movies",
+        path: "/movie/top_rated"
+    },
+
+]
+
 
 const genreList = {
     //create genre string from genre_id
@@ -121,6 +142,14 @@ const heroBanner = function ({ results: movieList }) {
 
     addHeroSlide();
 
+    /**
+     * fetch data for home page sections (top rated, upcoming, trending)
+     */
+
+    for (const { title, path } of homePageSections) {
+        fetchDataFromServer(`https://api.themoviedb.org/3${path}?api_key=${api_key}&page=1`, createMovieList, title)
+    }
+
 }
 
 
@@ -156,5 +185,30 @@ const addHeroSlide = function () {
 
 
     addEventOnElements(sliderControls, "click", sliderStart);
+}
+
+const createMovieList = function ({ results: movieList }, title) {
+    const movieListElem = document.createElement("section");
+    movieListElem.classList.add("movie-list");
+    movieListElem.ariaLabel = `${title}`;
+
+    movieListElem.innerHTML = `
+        <div class="title-wrapper">
+            <h3 class="title-large">${title}</h3>
+        </div>
+
+        <div class="slider-list">
+            <div class="slider-inner"></div>
+        </div>
+    `
+
+    for (const movie of movieList) {
+        const movieCard = createMovieCard(movie)
+
+        movieListElem.querySelector(".slider-inner").append(movieCard)
+    }
+
+
+    pageContent.appendChild(movieListElem)
 }
 
